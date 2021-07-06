e9d804e8d3e4d309b88fe7bfe782946533a8af5c
Gabriel Santana <gabriellima36716@gmail.com>
2021-07-05 23:37:45 -0300

    Criando as classes das tabelas no Flask

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #AQUI NÓS IMPORTAMOS TODAS AS BIBLIOTECAS QUE VAMOS PRCISAR DURANTE O DESENVOLVIMENTO DO NOSSO PROJETO
+from os import name
 from flask import (Flask, Blueprint, render_template, request)
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,4 +18,69 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secreta'
 db = SQLAlchemy(app)
 
+#MODELO
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_name = db.Column(db.String(60), nullable = False)
+    password_ = db,Column(db.String(16), nullable = False)
 
+    def __init__(self, user_name, password_):
+        self.user_name = user_name
+        self.password_ = password_
+
+    @staticmethod
+    def read_single(user_name):
+        return Users.query.get(user_name)
+
+class Pacientes1(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name_ = db.Column(db.String(60), nullable = False)
+    idade = db.Column(db.Int, nullable = False)
+    estado = db.Column(db.String(2), nullable = False)
+    sexo = db.Column(db.String(1), nullable = False)
+    imagem_url = db.Column(db.String(255), nullable = False)
+    profi = db.Column(db.String(255), nullable = True)
+
+    def __init__(self, name_, idade, estado, sexo, imagem_url, profi):
+        self.name_ = name_
+        self.idade = idade
+        self.estado = estado
+        self.sexo = sexo
+        self.imagem_url = imagem_url
+        self.profi = profi
+    
+    @staticmethod
+    def read_all():
+        return Pacientes1.query.order_by(Pacientes1.id.asc()).all()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+class Pacientes2(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name_ = db.Column(db.String(60), nullable = False)
+    idade = db.Column(db.Int, nullable = False)
+    estado = db.Column(db.String(2), nullable = False)
+    sexo = db.Column(db.String(1), nullable = False)
+    imagem_url = db.Column(db.String(255), nullable = False)
+    profi = db.Column(db.String(255), nullable = True)
+
+    def __init__(self, name_, idade, estado, sexo, imagem_url, profi):
+        self.name_ = name_
+        self.idade = idade
+        self.estado = estado
+        self.sexo = sexo
+        self.imagem_url = imagem_url
+        self.profi = profi
+    
+    @staticmethod
+    def read_all():
+        return Pacientes2.query.order_by(Pacientes2.id.asc()).all()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+        
